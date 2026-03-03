f32791625de7f49e8c70e18832522bb758e08086
Pedro Henrique <pedro2006batista@gmail.com>
2026-03-03 18:56:26 -0300

    repeticao com while

--- Estruturas.py
+++ Estruturas.py
@@ -1,7 +1,16 @@
 numContador = int(input("Digite o número da tabuada: "))
 numQuantidade = int(input("Ate qual Numero? "))
+contador = 0
 
 
 for i in range(numQuantidade + 1):  
     resultado = numContador * i
     print(f"{numContador} x {i} = {resultado}")
+
+
+print ("Repeticao usando while")
+
+while  contador < numQuantidade :
+    resultado2 = numContador * contador
+    contador = contador + 1
+    print(f"{numContador} x {contador} = {resultado2}")
