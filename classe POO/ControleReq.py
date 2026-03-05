8b68ddd889c97fa2a652b0d9d858d976f673a525
Pedro Henrique <pedro2006batista@gmail.com>
2026-03-04 22:30:02 -0300

    update controle de requisicoes

--- classe POO/ControleReq.py
+++ classe POO/ControleReq.py
@@ -5,7 +5,7 @@ class Requisicao:
         self.entregador = entregador
         self.data = data
 
-    def Mostrar_Info (self):
+    def mostrar_info (self):
         print(f"numero: {self.numero}")
         print(f"recebidor: {self.recebidor}")
         print(f"entregador: {self.entregador}")
@@ -15,38 +15,28 @@ lista_requisicoes = []
 condicao = True
 
 while condicao:
-
     continuar = 's'
-
-    if(continuar.lower != 's'):
-
-        try:
-            print("-----------------------------------")
-            numero = int(input("Numero Req: "))
-            recebidor = input("Quem Recebeu: ")
-            entregador = input("Quem Entregou: ")
-            data = input("Data de entrega: ")
-            print("-----------------------------------") 
-
-            req = Requisicao(numero,recebidor,entregador,data)
-            lista_requisicoes.append(req)
-
-            continuar = input("deseja continuar? s/n ")
-            if (continuar == 's'):
-                continue
-            else:
-                condicao = False
-
-
-
-        except:
-            print("erro de digitacao")
-            continuar = input("Deseja Continuar? s/n")
-
-            if continuar.lower() == 's':
-                continue
-            else:
-                condicao = False
+    try:
+        print("-----------------------------------")
+        numero = int(input("Numero Req: "))
+        recebidor = input("Quem Recebeu: ")
+        entregador = input("Quem Entregou: ")
+        data = input("Data de entrega: ")
+        print("-----------------------------------") 
+        req = Requisicao(numero,recebidor,entregador,data)
+        lista_requisicoes.append(req)
+        continuar = input("deseja continuar? s/n ")
+        if (continuar == 's'):
+            continue
+        else:
+            condicao = False
+    except ValueError:
+        print("erro de digitacao")
+        continuar = input("Deseja Continuar? s/n")
+        if continuar.lower() == 's':
+            continue
+        else:
+            condicao = False
 
 
 print("\nREQUISICOES CADASTRADAS\n")
@@ -54,5 +44,5 @@ print("\nREQUISICOES CADASTRADAS\n")
 for r in lista_requisicoes:
     print("------------------------")
     print()
-    r.Mostrar_Info()
+    r.mostrar_info()
     print("------------------------")
